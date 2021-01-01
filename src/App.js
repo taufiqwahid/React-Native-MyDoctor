26b8bcb777e9b9da48966b6813caa91285e9b603
Andi Muhammad Taufiq Wahid S <taufiq.wahid58@gmail.com>
2021-01-01 15:26:01 +0800

    splash scree

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Splash} from './pages';
+
+const App = () => {
+  return <Splash />;
+};
+
+export default App;
---
+++ src/pages/Splash/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {ILLogo} from '../../assets';
+
+const Splash = () => {
+  return (
+    <View style={styles.page}>
+      <ILLogo />
+      <Text style={styles.title}>My Doctor</Text>
+    </View>
+  );
+};
+
+export default Splash;
+
+const styles = StyleSheet.create({
+  page: {
+    backgroundColor: 'white',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    marginTop: 20,
+    fontSize: 20,
+    color: '#112340',
+  },
+});
