5b81a3edab13725e8e9448575932de9558e38ef3
Taufiq Wahid <taufiq.wahid58@gmail.com>
2021-10-30 15:23:29 +0800

    .

--- src/config/firebase/index.js
+++ src/config/firebase/index.js
@@ -1,13 +1,13 @@
 import * as firebase from 'firebase';
 
 var firebaseConfig = {
-  apiKey: 'AIzaSyBdxEOm1TzsvY_wCJW03YXsuBWtMIDVVK8',
-  authDomain: 'mydoctor-7fc6c.firebaseapp.com',
-  projectId: 'mydoctor-7fc6c',
-  storageBucket: 'mydoctor-7fc6c.appspot.com',
-  messagingSenderId: '364886111573',
-  appId: '1:364886111573:web:03d152586978f28ef76bdc',
-  measurementId: 'G-74S867XGY6',
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: '',
 };
 // Initialize Firebase
 // firebase.initializeApp(firebaseConfig);
